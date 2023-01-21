8127c680f527d062fbf3cdfe1a13d86e529b6c02
ATHAS1 <aayalas1602@alumno.ipn.mx>
2023-01-20 18:35:50 -0600

    php

--- Docking/Docking/doc.php
+++ Docking/Docking/doc.php
@@ -1,14 +1,11 @@
 <?php
 
 $dir = "Docdoc/";
-$tamanio= 200; //kb
 $permitidos= array('pdb','sdf');
 $ruta_carga = $dir . $_FILES['Archivo']['name'];
 $arregloArchivo= explode(".", $_FILES['Archivo']['name']);
 $extension = strtolower(end($arregloArchivo));// .pdb .sdf
 
-if(in_array($extension, $permitidos)){
-    if($__FILES ['Archivo']['size']< ($tamanio*1024)){
         if(!file_exists($dir)){
             mkdir($dir, 0777);
         }
@@ -17,13 +14,7 @@ if(in_array($extension, $permitidos)){
         } else {
             echo "Error al cargar el archivo";
         }
-    } else{
-        echo "Archivo excede el tamaño permitido";
-    }
-} else {
-    echo "Archivo no permitido";
-}
-
-
+    
+   
 
 ?>
