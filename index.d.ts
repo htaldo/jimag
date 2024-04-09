39dfaa272c5bf58aee238bee3e691e3f5607f59c
htaldo <htaldo0@gmail.com>
2024-04-08 20:03:32 -0600

    working pocket selector

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 declare module 'molstar/build/viewer/molstar';
 declare module 'molstar/build/viewer/molstar/mol-plugin-ui/react18';
+declare module 'molstar/build/viewer/molstar/mol-plugin/behavior/static/state';
--- static/jobs/chains.ts
+++ static/jobs/chains.ts
@@ -11,6 +11,7 @@ import { MolScriptBuilder as MS } from 'molstar/lib/mol-script/language/builder'
 import { Expression } from 'molstar/lib/mol-script/language/expression';
 import { PluginStateObject as SO } from 'molstar/lib/mol-plugin-state/objects';
 import { StateObjectRef } from 'molstar/lib/mol-state';
+import { setSubtreeVisibility } from 'molstar/lib/mol-plugin/behavior/static/state';
 
 import {
     Structure,
@@ -37,222 +38,207 @@ const MySpec: PluginUISpec = {
     ...DefaultPluginUISpec(),
     config: [
         [PluginConfig.VolumeStreaming.Enabled, false],
-        [PluginConfig.Viewport.ShowControls, false],
+        [PluginConfig.Viewport.ShowControls, true],
         [PluginConfig.Viewport.ShowSelectionMode, false],
         [PluginConfig.Viewport.ShowExpand, true],
         [PluginConfig.Viewport.ShowSettings, false],
-        [PluginConfig.Viewport.ShowTrajectoryControls, false]
+        [PluginConfig.Viewport.ShowTrajectoryControls, true]
     ],
     layout: {
 	initial: { //more in viewer/app.ts
 	    isExpanded: false,
 	    regionState: {
 		top: "hidden",
-		left: "hidden",
-		right: "hidden",
+		left: "full",
+		right: "full",
 		bottom: "hidden",
 	    },
 	},
     },
 }
 
-let loadReceptor;
-    
-(function() {
-    //let plugin = null; 
-    let plugin: PluginUIContext; 
+//let plugin = null; 
+let plugin: PluginUIContext; 
 
-    const MyComponent: React.FC = () => {
-	const [selected, setSelected] = React.useState([]);
+const MyComponent: React.FC = () => {
+    const [selected, setSelected] = React.useState([]);
 
-	React.useEffect(() => {
-	    async function initializePlugin() {
-		const parent = document.getElementById('chainsviewer')
-		if (!parent) return;
-
-		plugin = await createPluginUI(parent, MySpec);
-
-		//const receptor = 'https://files.rcsb.org/download/1a3n.pdb';
-		//const data = await plugin.builders.data.download({ url: receptor }, { state: { isGhost: true } });
-		//const trajectory = await plugin.builders.structure.parseTrajectory(data, 'pdb');
-		//await plugin.builders.structure.hierarchy.applyPreset(trajectory, 'default');
-
-		plugin.managers.interactivity.setProps({granularity: 'chain'});
-		plugin.selectionMode=true;
-		plugin.behaviors.interaction.click.subscribe(
-		    (event: InteractivityManager.ClickEvent) => {
-			const selections = Array.from(
-			    plugin.managers.structure.selection.entries.values()
-			);
-			//console.log("selections: ", selections);
-			// This bit can be customized to record any piece information you want
-			const localSelected: any[] = [];
-			for (const { structure } of selections) {
-			    //console.log("structure: ", structure);
-			    if (!structure) continue;
-			    Structure.eachAtomicHierarchyElement(structure, {
-				chain: (loc) => {
-				    const chainid = StructureProperties.chain.auth_asym_id(loc);
-				    localSelected.push({ chainid });
-				},
-			    });
-			}
-			setSelected(localSelected);
-			const chainString = localSelected.map(item => item.chainid).join(',');
-			console.log("chainString: ", chainString);
-			const stringDiv = document.getElementById('chainstring');
-			stringDiv.innerHTML = chainString;
-		    }
-		);
-	    }
-
-	    initializePlugin();
-	    return () => {
-		//TODO: cleanup
-	    };
-	}, []);
-
-	return null;
-    }
-
-    let currentStructure: StateObjectRef<SO.Molecule.Structure>;
-
-    //https://stackoverflow.com/questions/50176213/accessing-exported-functions-from-html-file
-    window.loadReceptor = async (receptor) => {
-	if (!plugin) {
-	    const parent = document.getElementById('chainsviewer');
+    React.useEffect(() => {
+	async function initializePlugin() {
+	    const parent = document.getElementById('chainsviewer')
 	    if (!parent) return;
+
 	    plugin = await createPluginUI(parent, MySpec);
-	     
-	} else {
-	    const stringDiv = document.getElementById('chainstring');
-	    stringDiv.innerHTML = '';
-	    plugin.clear();
-	    //console.log(receptor);
-	    //de la definición de loadStructureFromData	(viewer/app.ts)
-	    const rawdata = await plugin.builders.data.rawData({ data: receptor });
-	    const trajectory = await plugin.builders.structure.parseTrajectory(rawdata, 'pdbqt');
-	    await plugin.builders.structure.hierarchy.applyPreset(trajectory, 'default');
-	    const model = await plugin.builders.structure.createModel(trajectory);
-	    currentStructure = await plugin.builders.structure.createStructure(model);
-	}
-    }
 
-    function createSequenceIdExpression(
-	chain: string,
-	sequenceIds: number[],
-    ): Expression {
-	const query: any = {
-	    "residue-test": MS.core.set.has([
-		MS.set(...sequenceIds),
-		MS.ammp("auth_seq_id"),
-	    ]),
-	    "group-by": MS.struct.atomProperty.macromolecular.residueKey(), //what does this do?
+	    //const receptor = 'https://files.rcsb.org/download/1a3n.pdb';
+	    //const data = await plugin.builders.data.download({ url: receptor }, { state: { isGhost: true } });
+	    //const trajectory = await plugin.builders.structure.parseTrajectory(data, 'pdb');
+	    //await plugin.builders.structure.hierarchy.applyPreset(trajectory, 'default');
+
+	    plugin.managers.interactivity.setProps({granularity: 'chain'});
+	    plugin.selectionMode=true;
+	    plugin.behaviors.interaction.click.subscribe(
+		(event: InteractivityManager.ClickEvent) => {
+		    const selections = Array.from(
+			plugin.managers.structure.selection.entries.values()
+		    );
+		    //console.log("selections: ", selections);
+		    // This bit can be customized to record any piece information you want
+		    const localSelected: any[] = [];
+		    for (const { structure } of selections) {
+			//console.log("structure: ", structure);
+			if (!structure) continue;
+			Structure.eachAtomicHierarchyElement(structure, {
+			    chain: (loc) => {
+				const chainid = StructureProperties.chain.auth_asym_id(loc);
+				localSelected.push({ chainid });
+			    },
+			});
+		    }
+		    setSelected(localSelected);
+		    const chainString = localSelected.map(item => item.chainid).join(',');
+		    console.log("chainString: ", chainString);
+		    const stringDiv = document.getElementById('chainstring');
+		    stringDiv.innerHTML = chainString;
+		}
+	    );
+	}
+
+	initializePlugin();
+	return () => {
+	    //TODO: cleanup
 	};
-	query["chain-test"] = MS.core.rel.eq([
-	    MS.struct.atomProperty.macromolecular.auth_asym_id(),
-	    chain,
-	]);
+    }, []);
 
-	return MS.struct.generator.atomGroups(query);
+    return null;
+}
+
+let currentStructure: StateObjectRef<SO.Molecule.Structure>;
+
+//https://stackoverflow.com/questions/50176213/accessing-exported-functions-from-html-file
+window.loadReceptor = async (receptor) => {
+    const stringDiv = document.getElementById('chainstring');
+    stringDiv.innerHTML = '';
+    plugin.clear();
+    //console.log(receptor);
+    //de la definición de loadStructureFromData	(viewer/app.ts)
+    const rawdata = await plugin.builders.data.rawData({ data: receptor });
+    const trajectory = await plugin.builders.structure.parseTrajectory(rawdata, 'pdbqt');
+    await plugin.builders.structure.hierarchy.applyPreset(trajectory, 'default');
+    const model = await plugin.builders.structure.createModel(trajectory);
+    currentStructure = await plugin.builders.structure.createStructure(model);
+}
+
+function createSequenceIdExpression(
+    chain: string,
+    sequenceIds: number[],
+): Expression {
+    const query: any = {
+	"residue-test": MS.core.set.has([
+	    MS.set(...sequenceIds),
+	    MS.ammp("auth_seq_id"),
+	]),
+	"group-by": MS.struct.atomProperty.macromolecular.residueKey(), //what does this do?
+    };
+    query["chain-test"] = MS.core.rel.eq([
+	MS.struct.atomProperty.macromolecular.auth_asym_id(),
+	chain,
+    ]);
+
+    return MS.struct.generator.atomGroups(query);
+}
+
+window.togglePocketRepr = async (pocket:Pocket, isChecked: boolean) => {
+    if (isChecked) {
+	pocketReprOn(pocket);
+    } else {
+	pocketReprOff(pocket);
     }
+}
 
-    window.togglePocketRepr = async (pocket:Pocket, isChecked: boolean) => {
-	if (isChecked) {
-	    pocketReprOn(pocket);
-	} else {
-	    pocketReprOff(pocket);
-	}
-    }
+let loadedReprs = [];
+let components = {};
+//let queries: {[key:string]: any}= {};
+let structures: {[key:string]: any}= {};
 
-    let loadedReprs = {};
-    let components = {};
-    let queries: {[key:string]: any}= {};
-
-    const pocketReprOn = (pocket: Pocket) => {
-	console.log(pocket.rank);
-	console.log(loadedReprs);
-	console.log(loadedReprs[pocket.rank]);
-	if (loadedReprs[pocket.rank]) {
-	    for (const repr of loadedReprs[pocket.rank]) {
-		//repr.setState({ visible:true });	
-		    plugin.build()
-		    .to(repr)
-		    .update(createStructureRepresentationParams(plugin, undefined, {
-			type: 'molecular-surface',
-			typeParams: { alpha: 0.5 },
-			color: 'uniform',
-			colorParams: { value: Color(0x86F1E9) }
-		}))
-		.commit();
-	    }
-	} else {
-	    //loadedReprs[pocket.rank] = createPocketRepr(pocket);
-	    createPocketRepr(pocket);
-	    console.log(loadedReprs[pocket.rank]);
-	}
-    }
-
-    const pocketReprOff = (pocket: Pocket) => {
-	//console.log("pocket description: ", JSON.stringify(loadedReprs[pocket.rank], null, 2));
-	//loadedReprs[pocket.rank].forEach(function(repr: Representation.Any) {
-	//    console.log(repr);
-	//    repr.setState({visible: false});
-	//});
-	console.log(pocket.rank);
-	console.log(loadedReprs);
-	console.log(loadedReprs[pocket.rank]);
+const pocketReprOn = (pocket: Pocket) => {
+    if (loadedReprs[pocket.rank]) {
+	const updates = plugin.build();
 	for (const repr of loadedReprs[pocket.rank]) {
-	    plugin.build()
-		.to(repr)
+	    updates.to(repr)
 		.update(createStructureRepresentationParams(plugin, undefined, {
 		    type: 'molecular-surface',
-		    typeParams: { alpha: 0 },
+		    typeParams: { alpha: 0.5 },
 		    color: 'uniform',
 		    colorParams: { value: Color(0x86F1E9) }
-	    }))
-	    .commit();
+	    }));
 	}
+	updates.commit();
+
+    } else {
+	//loadedReprs[pocket.rank] = createPocketRepr(pocket);
+	createPocketRepr(pocket);
+	console.log(loadedReprs[pocket.rank]);
     }
+}
 
-    //return an array of representations, each corresponding to a subSeq of the pocket	
-    async function createPocketRepr (pocket: Pocket) {
-	//this is called assuming the Repr doesn't exist, so no need to check for existence
-	//let copy = { ... loadedReprs[pocket.rank]}; //DEBUG
-	//https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
-	for (const item of pocket.seq) {
-	    let chain = item.chain;
-	    let subSeq = item.subSeq;
-	    let key = `${pocket.rank}${chain}`; //components key
-	    console.log("CURRENT CHAIN, SUBSEQ: ", chain, subSeq); //DEBUG
-	    queries[key] = createSequenceIdExpression(chain, subSeq);
-	    components[key] = await plugin.builders.structure.tryCreateComponentFromExpression(currentStructure, queries[key], 'pocket${pocket.rank}${chain}');
-	    console.log("CURRENT STRUCTURE", currentStructure); //DEBUG
-	    let compcopy = {... components}; //DEBUG
-	    console.log("COMPONENTS", compcopy); //DEBUG
-	    console.log(`COMPONENTS[${key}]:`, compcopy[key]); //DEBUG
-	    let lrcopy = {... loadedReprs}; //DEBUG
-	    console.log(`loadedReprs[${key}] BEFORE: `, lrcopy[key]); //DEBUG
-
-	    let selector = await plugin.builders.structure.representation.addRepresentation(components[key], {
+const pocketReprOff = (pocket: Pocket) => {
+    const updates = plugin.build();
+    for (const repr of loadedReprs[pocket.rank]) {
+	updates.to(repr)
+	    .update(createStructureRepresentationParams(plugin, undefined, {
 		type: 'molecular-surface',
-		typeParams: { alpha: 0.5 },
+		typeParams: { alpha: 0 },
 		color: 'uniform',
 		colorParams: { value: Color(0x86F1E9) }
-	    });
-
-	    console.log(selector)
-	    loadedReprs[key] = selector;
-	    lrcopy = {... loadedReprs}; //DEBUG
-	    console.log(`loadedReprs[${key}] AFTER: `, lrcopy[key]); //DEBUG
-	}
-	//the following results in cyclic object value:
-	//console.log("pocket description (createPocketRepr): ", JSON.stringify(loadedReprs[pocket.rank], null, 2));
+	}));
     }
+    updates.commit();
+}
 
-    ReactDOM.render(
-	React.createElement(MyComponent),
-	//<MyComponent />,
-	document.getElementById('chainsviewer')
-    );
-})();
+//return an array of representations, each corresponding to a subSeq of the pocket	
+async function createPocketRepr (pocket: Pocket) {
+    //this is called assuming the Repr doesn't exist, so no need to check for existence
+    loadedReprs[pocket.rank] = [];
+    for (const item of pocket.seq) {
+	let chain = item.chain;
+	let subSeq = item.subSeq;
+	let key = `${pocket.rank}${chain}`; //components key, like 1A, 1B
+	let query = createSequenceIdExpression(chain, subSeq);
+	components[key] = await plugin.builders.structure.tryCreateComponentFromExpression(currentStructure, query, `pocket${key}`);
+
+	let selector = await plugin.builders.structure.representation.addRepresentation(components[key], {
+	    type: 'molecular-surface',
+	    typeParams: { alpha: 0.5 },
+	    color: 'uniform',
+	    colorParams: { value: Color(0x86F1E9) }
+	});
+
+	loadedReprs[pocket.rank].push(selector);
+    }
+}
+
+async function pause(ms) {
+    return new Promise(resolve => {
+    setTimeout(resolve, ms);
+    });
+}
+
+async function pauseTilEnter(comment: string) {
+    console.log("pause: ", comment);
+    return new Promise(resolve => {
+	const handleKeyPress = event => {
+	    if (event.key === 'Enter') {
+		window.removeEventListener('keydown', handleKeyPress);
+		resolve();
+	    } 
+	};
+	window.addEventListener('keydown', handleKeyPress);
+    });
+}
+
+ReactDOM.render(
+    React.createElement(MyComponent),
+    //<MyComponent />,
+    document.getElementById('chainsviewer')
+);
